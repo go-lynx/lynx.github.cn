7f03f2a9f19e14e5ff2c94db2fce2ab1a8a21603
tan-zhuo <tanzhuo@aliyun.com>
2023-12-04 19:31:00 +0800

    feat:Adjust the content of the document

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -66,7 +66,7 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'Lynx',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
@@ -80,7 +80,7 @@ const config = {
           },
           {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://github.com/facebook/docusaurus',
+            href: 'https://github.com/go-lynx/lynx',
             label: 'GitHub',
             position: 'right',
           },
