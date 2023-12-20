855e859d78f5ef035ad38ca114458dfb19129b04
tan-zhuo <tanzhuo@aliyun.com>
2023-12-20 15:26:47 +0800

    fix:Improve some settings

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,18 +13,19 @@ const config = {
   favicon: 'img/logo.png',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://go-lynx.cn',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'lynx', // Usually your GitHub org /user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'go-lynx', // Usually your GitHub org /user name.
+  projectName: 'lynx.github.cn', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
+  deploymentBranch: 'gh-pages',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
@@ -61,7 +62,7 @@ const config = {
   ],
 
   themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+  /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
