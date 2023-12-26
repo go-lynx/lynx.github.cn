acdf4c7d0e2e5bbe6d81cdf8f78a84d3f2a24a80
tan-zhuo <tanzhuo@aliyun.com>
2023-12-26 10:07:21 +0800

    feat:Improve docs.md

--- src/pages/index.js
+++ src/pages/index.js
@@ -32,7 +32,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg started-button"
-            to="/docs/intro">
+            to="/docs/docs">
             Get Started
           </Link>
           <Link
