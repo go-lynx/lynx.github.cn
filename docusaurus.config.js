8249cb278bb208921f6a73509f90b72d4b1e4786
tan-zhuo <tanzhuo@aliyun.com>
2024-01-03 11:04:32 +0800

    feat:Improve docs.md

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -83,7 +83,7 @@ const config = {
         items: [
           {
             type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
+            sidebarId: 'docs',
             position: 'left',
             label: 'Docs',
           },
